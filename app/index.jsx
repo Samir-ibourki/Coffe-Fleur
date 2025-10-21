d216c7fb495bcb4b492710a5a72b5e583e75fd66
Samir-ibourki <samiribourki7@gmail.com>
2025-10-21 22:23:50 +0100

    first commit in my branch

--- app/index.jsx
+++ app/index.jsx
@@ -1,30 +1,59 @@
-import { Text } from '@react-navigation/elements';
-import bg from '@/assets/images/bg.jpeg'
-import { ImageBackground, StyleSheet, View } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import { Link } from "expo-router";
+
+import bg from "../assets/images/bgg.jpeg";
+
 export default function HomeScreen() {
- return (
+  return (
     <View style={styles.container}>
       <ImageBackground source={bg} style={styles.bgImage} resizeMode="cover">
-        <Text style={styles.text}>hello world</Text>
+        <View style={styles.bottomContainer}>
+          <Text style={styles.text}>
+            My first thought in the morning is always yo
+          </Text>
+
+          <Link href="/product" style={styles.button}>
+            <Text style={styles.buttonText}>Go to Details</Text>
+          </Link>
+        </View>
       </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-  },
-  bgImage: {
-    flex: 1, 
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center', 
-    alignItems: 'center',
+  container: { flex: 1 },
+  bgImage: { flex: 1, justifyContent: "flex-end", paddingBottom: 50 },
+  bottomContainer: { 
+    alignItems: "center",       
+    justifyContent: "center",  
+    marginBottom:30
   },
   text: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
+    textShadowColor: "#000",
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 5,
+    maxWidth: "95%",       // <-- max width 90% dyal container
+  alignSelf: "center"
   },
+  button: {
+    backgroundColor: "#D2691E",
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
+  
+  },
+  buttonText: { color: "#fff", fontSize: 18, fontWeight: "bold", },
 });
